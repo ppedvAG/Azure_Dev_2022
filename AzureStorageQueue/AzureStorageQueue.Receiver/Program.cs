2f70d45f163fab57a47f0b8391139c13e0486b1b
André <AndreR@ppedv.de>
2022-06-13 11:40:32 +0200

    Azure Queue Sender und Empfänger

---
+++ AzureStorageQueue/AzureStorageQueue.Receiver/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Azure.Storage.Queues;
+
+Console.WriteLine("*** Azure Queue Receiver ***");
+
+
+var sasConString = "BlobEndpoint=https://mystorageandre.blob.core.windows.net/;QueueEndpoint=https://mystorageandre.queue.core.windows.net/;FileEndpoint=https://mystorageandre.file.core.windows.net/;TableEndpoint=https://mystorageandre.table.core.windows.net/;SharedAccessSignature=sv=2021-06-08&ss=q&srt=sco&sp=rwdlacup&se=2022-06-19T17:03:43Z&st=2022-06-13T09:03:43Z&spr=https&sig=ybCShDEHlP8zOmgFXec7QvAK7et4a67AOXEvSd3b9Vk%3D";
+
+var client = new QueueClient(sasConString, "hallowelt");
+
+
+while (true)
+{
+    var msg = client.ReceiveMessage();
+
+    if (msg.Value != null)
+    {
+        Console.WriteLine($"MSG {msg.Value.MessageId}: {msg.Value.Body}");
+        client.DeleteMessage(msg.Value.MessageId, msg.Value.PopReceipt);
+    }
+    else
+    {
+        Console.WriteLine("Keine neue MSG");
+        Thread.Sleep(1000);
+    }
+}
---
+++ AzureStorageQueue/AzureStorageQueue.Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Queues;
+
+namespace AzureStorageQueue.Sender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Queue Sender ***");
+
+            var sasConString = "BlobEndpoint=https://mystorageandre.blob.core.windows.net/;QueueEndpoint=https://mystorageandre.queue.core.windows.net/;FileEndpoint=https://mystorageandre.file.core.windows.net/;TableEndpoint=https://mystorageandre.table.core.windows.net/;SharedAccessSignature=sv=2021-06-08&ss=q&srt=sco&sp=rwdlacup&se=2022-06-19T17:03:43Z&st=2022-06-13T09:03:43Z&spr=https&sig=ybCShDEHlP8zOmgFXec7QvAK7et4a67AOXEvSd3b9Vk%3D";
+
+            var client = new QueueClient(sasConString, "hallowelt");
+
+            while (true)
+            {
+                string messageText = $"Es ist nun {DateTime.Now:G}:{DateTime.Now:ffff}";
+
+                var resp = client.SendMessage(messageText);
+
+                Console.WriteLine($"MSG {resp.Value.MessageId} {messageText} wurde versendet");
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
