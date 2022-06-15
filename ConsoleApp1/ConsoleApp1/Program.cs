ad6ae873a4de1200c747d68dca3707f007aa91de
André <AndreR@ppedv.de>
2022-06-15 12:40:08 +0200

    Logger mit serilog

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Serilog;
+
+Console.WriteLine("Hello, World!");
+
+
+Log.Logger = new LoggerConfiguration()
+                  .WriteTo.Seq("http://20.23.6.44")
+                  .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Month)
+                  .Enrich.WithMachineName()
+                  .Enrich.WithEnvironmentUserName()
+                  //.Enrich.WithExceptionDetails()
+                  .CreateLogger();
+
+Log.Warning("HILFEE!!!");
